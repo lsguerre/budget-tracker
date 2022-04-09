57064d4ac3809cafcb21660b9d1979d0cf82d17f
Opal <guerre5lily@gmail.com>
2022-04-09 15:39:04 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+app.use(logger("dev"));
+
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(routes);
+app.use(express.static("public"));
 
-// sync sequelize models to the database, then turn on the server
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
